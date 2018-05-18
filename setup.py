4c605b4ef26e96c7b16959c683f367c03246618f
Blake Huber <blake.huber@atos.net>
2018-05-18 16:40:06 -0500

    setup.py updated for metal

--- setup.py
+++ setup.py
@@ -1,19 +1,20 @@
 """
 
-bashtools :  Copyright 2018-2019, Blake Huber
+Metal :  Copyright 2018-2019, Blake Huber
 
-This program is free software: you can redistribute it and/or
-modify it under the terms of the MIT License as published by
-the Open Source Initiative.
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
 
-see: https://opensource.org/licenses/MIT
+see: https://www.gnu.org/licenses/#GPL
 
 This program is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
 
-You should have received a copy of the MIT License
+You should have received a copy of the GNU General Public License
 contained in the program LICENSE file.
 
 """
@@ -25,7 +26,7 @@ from setuptools.command.develop import develop
 from setuptools.command.install import install
 from subprocess import check_call
 from codecs import open
-import bashtools
+import metal
 
 
 requires = [
@@ -65,33 +66,33 @@ class PostInstall(install):
 
 
 setup(
-    name='bashtools',
-    version=bashtools.__version__,
-    description='Bash Tools for Amazon Web Services',
+    name='metal',
+    version=metal.__version__,
+    description='Security App Installation & Configuration',
     long_description=read('DESCRIPTION.rst'),
-    url='https://github.com/fstab50/bashtools',
-    author=bashtools.__author__,
-    author_email=bashtools.__email__,
+    url='https://github.com/fstab50/metal',
+    author=metal.__author__,
+    author_email=metal.__email__,
     license='MIT',
     classifiers=[
-        'Topic :: System :: Shells',
+        'Topic :: Security',
         'Topic :: System :: Systems Administration',
         'Topic :: Utilities',
         'Development Status :: 4 - Beta',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
-        'License :: OSI Approved :: MIT License',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
         'Operating System :: POSIX :: Linux',
     ],
-    keywords='bash Amazon Web Services AWS tools s3 ec2 lambda',
+    keywords='Security tools installation configure Amazon Web Services AWS tools s3',
     packages=find_packages(exclude=['docs', 'scripts', 'assets']),
     install_requires=requires,
     python_requires='>=3.4, <4',
     entry_points={
         'console_scripts': [
-            'bashtools=bashtools.cli:init_cli',
-            'keyconfig=bashtools.cli:option_configure'
+            'metal=metal.cli:init_cli',
+            'metalconfig=metal.cli:option_configure'
         ]
     },
     zip_safe=False
